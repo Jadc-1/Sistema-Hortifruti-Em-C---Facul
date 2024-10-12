648ef99eda2195415901b478c1cdaf5fb38def95
João Alexandre <joaoale.25@gmail.com>
2024-10-12 09:41:21 -0300

    update System.c

--- SistemaHortifruti.c
+++ SistemaHortifruti.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 #include <unistd.h>
 #include <string.h>
 #include <ctype.h>
 #include <windows.h>
+#include <locale.h>
 
 
 struct Produto {
@@ -13,34 +13,33 @@ struct Produto {
     float precoKG;
 };
 
-void CadastrarFruta(struct Produto informacoesProduto[], int *i) { // Chamei o vetor informacoesProduto, porém ele é definido no int main.
-
+void CadastrarFruta(struct Produto informacoesProduto[], int *i) { // Chamei o vetor informacoesProduto, porÃ©m ele Ã© definido no int main.
     char opcao;
-    refaz:informacoesProduto[*i].id = *i + 1; // aqui estou utilizando ponteiro para o valor ser mais preciso, pegando o endereço da memoria da variavel.
-    printf("ID: %d\n", informacoesProduto[*i].id);
-    printf("\nQual fruta você deseja cadastrar?");
+    refaz:informacoesProduto[*i].id = *i + 1; // Em C o ponteiro nÃ£o Ã© utilizado automaticamente para mudar a variavel original e pegar o seu endereÃ§o de memoria,  utilizamos *, para usar o enderaÃ§o, utilizamos %p, pegar o valor (%d, %f, %lf)
+    printf("ID: %d\n", informacoesProduto[*i].id); // Geralmente Ã© utilizado ponteiro com struct, para garantir que a variavel original serÃ¡ modificada e nÃ£o sÃ³ a cÃ³pia
+    printf("\nQual fruta vocÃª deseja cadastrar?");
     scanf("%s", informacoesProduto[*i].nome);
-    printf("\nAdicione o preço Unitário por KG: ");
+    printf("\nAdicione o preÃ§o UnitÃ¡rio por KG: ");
     scanf(" %f", &informacoesProduto[*i].precoKG);
     (*i)++;
     printf("\nQuer cadastrar mais algum produto? S/N\n");
-    fflush(stdin); // Limpa o buffer de entrada para que o %c não leia o \n
+    fflush(stdin); // Limpa o buffer de entrada para que o %c nï¿½o leia o \n
     scanf(" %c", &opcao);
-    opcao = toupper(opcao); // Função toupper da biblioteca ctype, para deixar a resposta em maiusculo
+    opcao = toupper(opcao); // FunÃ§Ã£o toupper da biblioteca ctype, para deixar a resposta em maiusculo
     if (opcao != 'S' && opcao != 'N') {
         printf("Incorreto...\n");
         printf("Digite S ou N: ");
         scanf(" %c", &opcao);
     } else if (opcao == 'S') {
         system("cls");
-        goto refaz; // goto para substituir os laços de repetição
+        goto refaz; // goto para substituir os laÃ§os de repetiÃ§Ã£o
     } else {
         system("cls");
     }
 
 }
 
-int Menu() { // Função do menu que retorna para o main a opção escolhida
+int Menu() { // FunÃ§Ã£o do menu que retorna para o main a opï¿½ï¿½o escolhida
     int op;
     linha();
     printf("            MENU PDV");
@@ -49,18 +48,20 @@ int Menu() { // Função do menu que retorna para o main a opção escolhida
     printf("2 - Sistema de Estoque\n");
     printf("3 - Sair\n");
     linha();
-    printf("Digite sua opção: ");
+    printf("Digite sua opÃ§Ã£o: ");
     scanf("%d", &op);
     system("cls");
     return op;
 }
 
-void linha() { // função de linha para organizar meu codigo
+void linha() { // funÃ§Ã£o de linha para organizar meu codigo
     printf("\n---------------------------------\n");
 }
 
 int main(void) {
-    setlocale(LC_ALL, "Portuguese");
+    //LC_ALL, "Portuguese" nÃ£o estÃ¡ rodando corretamente, entÃ£o estou utilizando estes 2 codigos
+    system("chcp 65001 > NULL"); // Troca a pagina para UTF-8 (Aceita qualquer acentuaÃ§Ã£o), coloquei > NULL para nÃ£o aparecer no console
+    setlocale(LC_ALL, "pt-br"); // Adicionei a localidade de pt-br
     struct Produto informacoesProduto[10];
     int i = 0;
 
@@ -78,26 +79,27 @@ int main(void) {
             sleep(1);
             system("cls");
             linha();
-            printf("Sistema de Estoque:");
+            printf("       Sistema de Estoque:");
             linha();
+            printf("\n");
             printf("1 - Cadastrar Produto\n");
             printf("2 - Listar Produtos\n");
             printf("3 - Voltar ao menu PDV\n");
-            int opCadastro;
-            printf("\nOpção desejada?\n");
-            scanf("%d", &opCadastro);
             linha();
+            int opCadastro;
+            printf("\nOpÃ§Ã£o desejada?\n");
+            scanf("%d", &opCadastro);
             switch (opCadastro) {
                 case 1:
                     system("cls");
-                    CadastrarFruta(informacoesProduto, &i); //Estou chamando a função criada e adicionando as variaveis necessárias para o seu funcionamento, sendo uma delas o endereço de memoria de i.
+                    CadastrarFruta(informacoesProduto, &i); //Estou chamando a funÃ§Ã£o criada e adicionando as variaveis necessï¿½rias para o seu funcionamento, sendo uma delas o endereï¿½o de memoria de i.
                     break;
                 case 2:
                     system("cls");
                     printf("--------------------------------------\n");
-                    printf("   ID:  |     Fruta:   |  Preço KG   |\n");
+                    printf("|  ID:  |     Fruta:   |  PreÃ§o KG   |\n");
                     printf("--------------------------------------\n");
-                    for (int fru = 0; fru < i; fru++) { //variavel fru, vai repetir o código enquanto for menor que i, no qual é incrementado quando adicionado um novo produto
+                    for (int fru = 0; fru < i; fru++) { //variavel fru, vai repetir o cÃ³digo enquanto for menor que i, no qual Ã© incrementado quando adicionado um novo produto
                         printf("| %-5d | %-12s |   %8.2f  |\n", // Eu defini tamanhos fixos para cada variavel, 5 caracteres para ID e 12 para string, para ficar aninhado
                                informacoesProduto[fru].id,
                                informacoesProduto[fru].nome,
@@ -109,6 +111,7 @@ int main(void) {
 
                     break;
                 case 3:
+                    printf("\n");
                     printf("Voltando...");
                     usleep(500000);
                     system("cls");
@@ -124,7 +127,7 @@ int main(void) {
         default:
             break;
         }
-    } while (op != 3); // Vai repetir enquanto a resposta for diferente de 3, se for 3, é a opção sair, então irá para de repetir
+    } while (op != 3); // Vai repetir enquanto a resposta for diferente de 3, se for 3,Ã© a opÃ§Ã£o sair, entÃ£o irÃ¡ parar de repetir
 
 
 }
